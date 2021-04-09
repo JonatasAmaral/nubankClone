f7ae5d72c674979269686ff6e3ee9ffb0a7a51f3
Jonatas Amaral <jonataspadtab@gmail.com>
2021-04-08 23:38:07 -0300

    criar header do app

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container } from './styles';
+import Header from '../../components/Header';
 
 export default function App() {
   return (
     <Container>
+      <Header />
     </Container>
   );
 }
