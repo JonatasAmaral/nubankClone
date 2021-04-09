380228a2630bed8bf46c5ca6ed1d66fe4d1c00f2
Jonatas Amaral <jonataspadtab@gmail.com>
2021-04-08 23:39:51 -0300

    criar tabs

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, TabItem, TabsContainer, TabText } from './styles';
+
+const tabsItens = [
+  { icon: 'person-add', text: 'Indicar amigos' },
+  { icon: 'chat-bubble-outline', text: 'Cobrar' },
+  { icon: 'arrow-downward', text: 'Depositar' },
+  { icon: 'arrow-upward', text: 'Transferir' },
+  { icon: 'lock', text: 'Broquear cartão' },
+]
+
+export default function Tabs() {
+  return (
+    <Container>
+      <TabsContainer>
+        {tabsItens.map((tab)=>{
+          return (
+            <TabItem>
+              <Icon name={tab.icon} size={24} color="#fff"></Icon>
+              <TabText>{tab.text}</TabText>
+            </TabItem>
+          )
+        })}
+        
+      </TabsContainer>
+    </Container>
+  );
+}
